08eb29dc5fa1c6aff616d0abd851b0b37ecb7453
AntonDolgov <dlgvnt@gmail.com>
2019-06-25 02:36:03 +0300

    header categories

--- src/components/header/headerDropdown.js
+++ src/components/header/headerDropdown.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { createPortal } from 'react-dom';
 import HeaderNavList from './headerNavList';
 
 export default class extends Component {
@@ -12,12 +13,25 @@ export default class extends Component {
         })
     }
 
+    onDropdownOverlayClick = () => {
+        this.setState({
+            isDropdownActive: false
+        })
+    }
+
     render () {
         const { isDropdownActive } = this.state;
 
         const dropdownClassName =
         `header__dropdown ${isDropdownActive ?
         'header__dropdown-btn--is-active' : ''}`;
+
+        const Overlay = () => {
+            return createPortal(
+                    <div className="header__dropdown-overlay" onClick={this.onDropdownOverlayClick}></div>,
+                    document.body
+                );
+        };
 
         return (
             <div className={dropdownClassName}>
@@ -29,6 +43,8 @@ export default class extends Component {
 
                 <div className="header__dropdown-content">
                     <HeaderNavList vertical/>
+
+                    {isDropdownActive ? <Overlay /> : ''}
                 </div>
             </div>);
 
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,6 +13,49 @@ const Header = () => (
                 <HeaderDropdown />
             </div>
         </nav>
+
+        <div className="header__main">
+            <div className="container">
+                <ul className="header__categories">
+                    <li className="header__categories-item">
+                        <button
+                            className="header__categories-btn"
+                            type="button"
+                            style={{backgroundImage: 'url(https://starwars-visualguide.com/assets/img/characters/1.jpg)'}}
+                            title="Characters">
+                                Characters
+                        </button>
+                    </li>
+                    <li className="header__categories-item">
+                        <button
+                            className="header__categories-btn"
+                            type="button"
+                            style={{backgroundImage: 'url(https://starwars-visualguide.com/assets/img/starships/5.jpg)'}}
+                            title="Starships">
+                                Starships
+                        </button>
+                    </li>
+                    <li className="header__categories-item">
+                        <button
+                            className="header__categories-btn"
+                            type="button"
+                            style={{backgroundImage: 'url(https://starwars-visualguide.com/assets/img/vehicles/8.jpg)'}}
+                            title="Vehicles">
+                                Vehicles
+                        </button>
+                    </li>
+                    <li className="header__categories-item">
+                        <button
+                            className="header__categories-btn"
+                            type="button"
+                            style={{backgroundImage: 'url(https://starwars-visualguide.com/assets/img/planets/2.jpg)'}}
+                            title="Planets">
+                                Planets
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
 );
 
