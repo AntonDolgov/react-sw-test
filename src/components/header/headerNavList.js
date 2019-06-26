dae3169a1ce268af74c7767cad1816789add21b9
AntonDolgov <dlgvnt@gmail.com>
2019-06-27 02:16:11 +0300

    nav link transition

--- src/components/header/headerNavList.js
+++ src/components/header/headerNavList.js
@@ -6,13 +6,14 @@ export default (props) => {
     return (
         <ul className={`header__nav-list ${vertical ? 'header__nav-list--vertical' : ''}`}>
             <li className="header__nav-item">
-                <a href="#id" className="header__nav-link">Link 1</a>
+                <a href="#id" className="header__nav-link">
+                    <span className="header__nav-link-text">Luke Skywalker's home</span></a>
             </li>
             <li className="header__nav-item">
-                <a href="#id" className="header__nav-link">Link 2</a>
+                <a href="#id" className="header__nav-link"><span className="header__nav-link-text">Luke Skywalker's info</span></a>
             </li>
             <li className="header__nav-item">
-                <a href="#id" className="header__nav-link">Link 3</a>
+                <a href="#id" className="header__nav-link"><span className="header__nav-link-text">Categories</span></a>
             </li>
         </ul>);
 };
